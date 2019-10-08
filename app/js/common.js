345668fbfb98f4919e683df4e341e845589fd8ec
Petr Petukhov <kotov0991@gmail.com>
2019-10-08 17:10:52 +0300

    header, footer desktop

--- app/js/common.js
+++ app/js/common.js
@@ -381,7 +381,7 @@ $(function() {
 
 
 
-
+	// Анимации кнопок
 	var animateCta = function(e) {
 
 		// Для демо
@@ -403,7 +403,19 @@ $(function() {
 	}
 
 
+	[].map.call(document.querySelectorAll('.btn'), el=> {
+		el.addEventListener('click',e => {
+			e = e.touches ? e.touches[0] : e;
+			const r = el.getBoundingClientRect(), d = Math.sqrt(Math.pow(r.width,2)+Math.pow(r.height,2)) * 2;
+			el.style.cssText = `--s: 0; --o: 1;`;  el.offsetTop; 
+			el.style.cssText = `--t: 1; --o: 0; --d: ${d}; --x:${e.clientX - r.left}; --y:${e.clientY - r.top};`
+		})
+	})
 
+	$('.menu-trigger').click(function (e) { 
+		$(this).toggleClass('menu-open');
+		$('.toggle-menu').toggleClass('menu-open');
+	});
 
 
 });
